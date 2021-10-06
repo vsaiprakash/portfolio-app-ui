0af10812bc56bb0f8180fde76cb8ccb15145c5d0
V. Sai Prakash <vukkumsp@gmail.com>
2021-10-06 13:50:31 +0000

    code cleanup;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portfolio-app-ui';
+
+  scrollToSectionId(sectionId: string): void {
+    let elmnt = document.getElementById(sectionId);
+    elmnt.scrollIntoView();
+  }
 }
--- src/app/sections/menu-bar/menu-bar.component.ts
+++ src/app/sections/menu-bar/menu-bar.component.ts
@@ -12,4 +12,5 @@ export class MenuBarComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
 }

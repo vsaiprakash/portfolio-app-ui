f6a94be1fb1ce51f7d0925d32e54f2614446e13a
V. Sai Prakash <vukkumsp@gmail.com>
2021-10-06 17:06:10 +0000

    added sectionController in all sections;

--- src/app/sections/contact-me/contact-me.component.ts
+++ src/app/sections/contact-me/contact-me.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SectionControllerService } from '../section-controller.service';
 
 @Component({
   selector: 'app-contact-me',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactMeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private sectionController: SectionControllerService) { }
 
   ngOnInit(): void {
   }
 
+  goToHeaderSection(){
+    this.sectionController.updateSection(0);
+  }
 }
--- src/app/sections/skills/skills.component.ts
+++ src/app/sections/skills/skills.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SectionControllerService } from '../section-controller.service';
 
 @Component({
   selector: 'app-skills',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SkillsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private sectionController: SectionControllerService) { }
 
   ngOnInit(): void {
+  }
+
+  goToHeaderSection(){
+    this.sectionController.updateSection(0);
   }
 
 }

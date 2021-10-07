5efec8ac3ca4c4096607357f8df7b93b79b23e8a
V. Sai Prakash <vukkumsp@gmail.com>
2021-10-07 17:55:07 +0000

    basic responsive cards for skills page added;

--- src/app/sections/skills/skills.component.ts
+++ src/app/sections/skills/skills.component.ts
@@ -8,7 +8,54 @@ import { SectionControllerService } from '../section-controller.service';
 })
 export class SkillsComponent implements OnInit {
 
-  constructor(private sectionController: SectionControllerService) { }
+  skillsList: any[];
+  skillsList4x: any[];
+  skillsList2x: any[];
+
+  constructor(private sectionController: SectionControllerService) {
+    this.skillsList = [
+      { title: "Angular", img: "assets/images/colleagues.svg" },
+      { title: "NodeJs", img: "assets/images/colleagues.svg" },
+      { title: "ExpressJs", img: "assets/images/colleagues.svg" },
+      { title: "Java", img: "assets/images/colleagues.svg" },
+      { title: "AWS", img: "assets/images/colleagues.svg" },
+      { title: "Firebase", img: "assets/images/colleagues.svg" }
+    ];
+    // let row4x = [];
+    // let row2x = [];
+    // let counter2x = 0;
+    // let counter4x = 0;
+    
+    // this.skillsList.forEach(skill => {
+
+    //   if(counter2x==2){
+    //     //reset the counter
+    //     counter2x = 0;
+    //     //assign the row to skillList2x
+    //     this.skillsList2x.push(row2x);
+    //     //reset row
+    //     row2x = [];
+    //   }
+
+    //   if(counter4x==4){
+    //     //reset the counter
+    //     counter4x = 0;
+    //     //assign the row to skillList4x
+    //     this.skillsList4x.push(row4x);
+    //     //reset row
+    //     row4x = [];
+    //   }
+
+    //   //build skillsList2x
+    //   row2x.push(skill);
+    //   counter2x++;
+
+    //   //build skillsList4x
+    //   row4x.push(skill);
+    //   counter4x++;
+
+    // });
+  }
 
   ngOnInit(): void {
   }

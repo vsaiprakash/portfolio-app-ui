8976791208a6b18b1f32077af9620364ec72199a
V. Sai Prakash <vukkumsp@gmail.com>
2021-10-09 05:51:01 +0000

    updates;

--- src/app/sections/skills/skills.component.ts
+++ src/app/sections/skills/skills.component.ts
@@ -8,55 +8,37 @@ import { SectionControllerService } from '../section-controller.service';
 })
 export class SkillsComponent implements OnInit {
 
-  skillsList: any[];
-  skillsList4x: any[];
-  skillsList2x: any[];
+  primarySkillsList: any[];
+  secondarySkillsList: any[];
 
   constructor(private sectionController: SectionControllerService) {
-    this.skillsList = [
+    this.primarySkillsList = [
       { title: "Angular", img: "assets/images/skills/angular-logo.svg" },
       { title: "NodeJs", img: "assets/images/skills/nodejs-logo.svg" },
       { title: "ExpressJs", img: "assets/images/skills/expressjs-logo.svg" },
-      { title: "Java", img: "assets/images/skills/java-logo.svg" },
       { title: "AWS", img: "assets/images/skills/aws-logo.svg" },
-      { title: "Firebase", img: "assets/images/skills/firebase-logo.svg" },
-      { title: "Postman", img: "assets/images/skills/postman-logo.svg" },
+      { title: "Bootstrap", img: "assets/images/skills/bootstrap-logo.svg" },
+      { title: "Javascript", img: "assets/images/skills/javascript-logo.svg" },
+      { title: "JQuery", img: "assets/images/skills/jquery-logo.svg" },
+
       // { title: "Insomnia", img: "assets/images/skills/insomnia-logo.svg" }
     ];
-    // let row4x = [];
-    // let row2x = [];
-    // let counter2x = 0;
-    // let counter4x = 0;
-    
-    // this.skillsList.forEach(skill => {
 
-    //   if(counter2x==2){
-    //     //reset the counter
-    //     counter2x = 0;
-    //     //assign the row to skillList2x
-    //     this.skillsList2x.push(row2x);
-    //     //reset row
-    //     row2x = [];
-    //   }
-
-    //   if(counter4x==4){
-    //     //reset the counter
-    //     counter4x = 0;
-    //     //assign the row to skillList4x
-    //     this.skillsList4x.push(row4x);
-    //     //reset row
-    //     row4x = [];
-    //   }
-
-    //   //build skillsList2x
-    //   row2x.push(skill);
-    //   counter2x++;
-
-    //   //build skillsList4x
-    //   row4x.push(skill);
-    //   counter4x++;
-
-    // });
+    this.secondarySkillsList = [
+      { title: "Core Java", img: "assets/images/skills/java-logo.svg" },
+      { title: "Maven", img: "assets/images/skills/maven-logo.svg" },
+      { title: "RDBMS", img: "assets/images/skills/rdbms-logo.svg" },
+      { title: "SpringBoot", img: "assets/images/skills/springboot-logo.svg" },
+      { title: "Firebase", img: "assets/images/skills/firebase-logo.svg" },
+      { title: "Postman", img: "assets/images/skills/postman-logo.svg" },
+      { title: "Git", img: "assets/images/skills/git-logo.svg" },
+      { title: "NPM", img: "assets/images/skills/npm-logo.svg" },
+      { title: "RabbitMQ", img: "assets/images/skills/rabbitmq-logo.svg" },
+      { title: "HTML", img: "assets/images/skills/html5-logo.svg" },
+      { title: "CSS", img: "assets/images/skills/css3-logo.svg" },
+      { title: "Less", img: "assets/images/skills/less-logo.svg" },
+      { title: "Serverless", img: "assets/images/skills/serverless-logo.svg" },
+    ];
   }
 
   ngOnInit(): void {

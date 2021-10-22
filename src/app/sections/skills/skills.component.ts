4c6da500a53e337f2a0a07a09c3a9a4a604d8440
V. Sai Prakash <vukkumsp@gmail.com>
2021-10-22 10:40:43 +0000

    skills data moved to skills service;

--- src/app/sections/skills/skills.component.ts
+++ src/app/sections/skills/skills.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SectionControllerService } from '../section-controller.service';
+import { SkillsService } from './skills.service';
 
 @Component({
   selector: 'app-skills',
@@ -11,37 +12,45 @@ export class SkillsComponent implements OnInit {
   primarySkillsList: any[];
   secondarySkillsList: any[];
 
-  constructor(private sectionController: SectionControllerService) {
-    this.primarySkillsList = [
-      { title: "Angular", img: "assets/images/skills/angular-logo.svg" },
-      { title: "NodeJs", img: "assets/images/skills/nodejs-logo.svg" },
-      { title: "ExpressJs", img: "assets/images/skills/expressjs-logo.svg" },
-      { title: "AWS", img: "assets/images/skills/aws-logo.svg" },
-      { title: "Bootstrap", img: "assets/images/skills/bootstrap-logo.svg" },
-      { title: "Javascript", img: "assets/images/skills/javascript-logo.svg" },
-      { title: "JQuery", img: "assets/images/skills/jquery-logo.svg" },
+  constructor(
+    private sectionController: SectionControllerService,
+    private skillsService: SkillsService) {
+    // this.primarySkillsList = [
+    //   { title: "Angular", img: "assets/images/skills/angular-logo.svg" },
+    //   { title: "NodeJs", img: "assets/images/skills/nodejs-logo.svg" },
+    //   { title: "ExpressJs", img: "assets/images/skills/expressjs-logo.svg" },
+    //   { title: "AWS", img: "assets/images/skills/aws-logo.svg" },
+    //   { title: "Bootstrap", img: "assets/images/skills/bootstrap-logo.svg" },
+    //   { title: "Javascript", img: "assets/images/skills/javascript-logo.svg" },
+    //   { title: "JQuery", img: "assets/images/skills/jquery-logo.svg" },
 
-      // { title: "Insomnia", img: "assets/images/skills/insomnia-logo.svg" }
-    ];
+    //   // { title: "Insomnia", img: "assets/images/skills/insomnia-logo.svg" }
+    // ];
 
-    this.secondarySkillsList = [
-      { title: "Core Java", img: "assets/images/skills/java-logo.svg" },
-      { title: "Maven", img: "assets/images/skills/maven-logo.svg" },
-      { title: "RDBMS", img: "assets/images/skills/rdbms-logo.svg" },
-      { title: "SpringBoot", img: "assets/images/skills/springboot-logo.svg" },
-      { title: "Firebase", img: "assets/images/skills/firebase-logo.svg" },
-      { title: "Postman", img: "assets/images/skills/postman-logo.svg" },
-      { title: "Git", img: "assets/images/skills/git-logo.svg" },
-      { title: "NPM", img: "assets/images/skills/npm-logo.svg" },
-      { title: "RabbitMQ", img: "assets/images/skills/rabbitmq-logo.svg" },
-      { title: "HTML", img: "assets/images/skills/html5-logo.svg" },
-      { title: "CSS", img: "assets/images/skills/css3-logo.svg" },
-      { title: "Sass", img: "assets/images/skills/sass-logo.svg" },
-      { title: "Serverless", img: "assets/images/skills/serverless-logo.svg" },
-    ];
+    // this.secondarySkillsList = [
+    //   { title: "Core Java", img: "assets/images/skills/java-logo.svg" },
+    //   { title: "Maven", img: "assets/images/skills/maven-logo.svg" },
+    //   { title: "RDBMS", img: "assets/images/skills/rdbms-logo.svg" },
+    //   { title: "SpringBoot", img: "assets/images/skills/springboot-logo.svg" },
+    //   { title: "Firebase", img: "assets/images/skills/firebase-logo.svg" },
+    //   { title: "Postman", img: "assets/images/skills/postman-logo.svg" },
+    //   { title: "Git", img: "assets/images/skills/git-logo.svg" },
+    //   { title: "NPM", img: "assets/images/skills/npm-logo.svg" },
+    //   { title: "RabbitMQ", img: "assets/images/skills/rabbitmq-logo.svg" },
+    //   { title: "HTML", img: "assets/images/skills/html5-logo.svg" },
+    //   { title: "CSS", img: "assets/images/skills/css3-logo.svg" },
+    //   { title: "Sass", img: "assets/images/skills/sass-logo.svg" },
+    //   { title: "Serverless", img: "assets/images/skills/serverless-logo.svg" },
+    // ];
   }
 
   ngOnInit(): void {
+    this.skillsService.getPrimarySkillsList().subscribe(res => {
+      this.primarySkillsList = res;
+    });
+    this.skillsService.getSecondarySkillsList().subscribe(res => {
+      this.secondarySkillsList = res;
+    });
   }
 
   goToHeaderSection(){

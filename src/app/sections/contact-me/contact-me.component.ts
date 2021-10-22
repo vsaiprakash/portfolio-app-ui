06e6aed30e25f23e1fdd066ec674d49c1731105b
V. Sai Prakash <vukkumsp@gmail.com>
2021-10-22 10:59:31 +0000

    socials data moved to contact me service;

--- src/app/sections/contact-me/contact-me.component.ts
+++ src/app/sections/contact-me/contact-me.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SectionControllerService } from '../section-controller.service';
+import { ContactMeService } from './contact-me.service';
 
 @Component({
   selector: 'app-contact-me',
@@ -10,16 +11,21 @@ export class ContactMeComponent implements OnInit {
 
   socialsList: any[];
 
-  constructor(private sectionController: SectionControllerService) {
-    this.socialsList = [
-      { title: "Github", img: "assets/images/socials/github-logo.svg", link: "https://github.com/vsaiprakash" },
-      { title: "LinkedIn", img: "assets/images/socials/linkedin-logo.svg", link: "https://www.linkedin.com/in/vsaiprakash" },
-      { title: "HackerRank", img: "assets/images/socials/hackerrank-logo.svg", link: "https://www.hackerrank.com/vukkumsp" },
-      { title: "Instagram", img: "assets/images/socials/instagram-logo.svg", link: "https://www.instagram.com/specteller" }
-    ]
+  constructor(
+    private sectionController: SectionControllerService,
+    private contactMeService: ContactMeService) {
+    // this.socialsList = [
+    //   { title: "Github", img: "assets/images/socials/github-logo.svg", link: "https://github.com/vsaiprakash" },
+    //   { title: "LinkedIn", img: "assets/images/socials/linkedin-logo.svg", link: "https://www.linkedin.com/in/vsaiprakash" },
+    //   { title: "HackerRank", img: "assets/images/socials/hackerrank-logo.svg", link: "https://www.hackerrank.com/vukkumsp" },
+    //   { title: "Instagram", img: "assets/images/socials/instagram-logo.svg", link: "https://www.instagram.com/specteller" }
+    // ]
   }
 
   ngOnInit(): void {
+    this.contactMeService.getSocialsList().subscribe(res => {
+      this.socialsList = res;
+    });
   }
 
   goToHeaderSection(){
---
+++ src/app/sections/contact-me/contact-me.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContactMeService {
+
+    socialsListSubject: BehaviorSubject<any[]> = new BehaviorSubject<any[]>(null);
+    socialsList$: Observable<any[]> = this.socialsListSubject.asObservable();
+
+    getSocialsList(): Observable<any>{
+        const socialsList = [
+            { title: "Github", img: "assets/images/socials/github-logo.svg", link: "https://github.com/vsaiprakash" },
+            { title: "LinkedIn", img: "assets/images/socials/linkedin-logo.svg", link: "https://www.linkedin.com/in/vsaiprakash" },
+            { title: "HackerRank", img: "assets/images/socials/hackerrank-logo.svg", link: "https://www.hackerrank.com/vukkumsp" },
+            { title: "Instagram", img: "assets/images/socials/instagram-logo.svg", link: "https://www.instagram.com/specteller" }
+        ];
+
+        this.socialsListSubject.next(socialsList);
+        return this.socialsList$;
+    }
+}
